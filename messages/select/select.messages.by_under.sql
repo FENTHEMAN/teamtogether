ffc8d12c4956f990dac612eb1cf9b9f46373263c
fen <fenfenus@MacBook-Air-fen.local>
2026-03-07 03:28:24 +0700

    [UPDATE] Update select query for by under messages for messages

--- messages/select/select.messages.by_under.sql
+++ messages/select/select.messages.by_under.sql
@@ -1,7 +1,5 @@
-SELECT 
-    id,
-    unders_message
-FROM (
+WITH
+unders_messages AS (
     SELECT
         messages.id,
         value AS unders_message
@@ -9,3 +7,22 @@ FROM (
     JOIN users ON messages.sender_id = users.id
     WHERE users.name = 'Under'
 )
+SELECT 
+    unders_messages.id,
+    unders_messages.unders_message
+FROM unders_messages;
+
+
+-- Or this format 
+
+-- SELECT 
+--     unders_messages.id,
+--     unders_messages.unders_message
+-- FROM (
+--     SELECT
+--         messages.id,
+--         value AS unders_message
+--     FROM messages
+--     JOIN users ON messages.sender_id = users.id
+--     WHERE users.name = 'Under'
+-- ) AS unders_messages;
