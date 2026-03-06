f61930973a9e27d8fdbd93e5b5a75e7b1ac77685
fen <fenfenus@MacBook-Air-fen.local>
2026-03-07 01:20:55 +0700

    [FEATURE] Add basic insertion add selection for items

---
+++ items/insert/insert.items.main.sql
@@ -0,0 +1,9 @@
+INSERT INTO items
+    (name, rare, owner_id, price, is_selling)
+VALUES
+    ('Rotter Magic Axe', 'legendary', 3, 50, true),
+    ('Poop', 'common', 1, 1, true),
+    ('Trash Trinket', 'uncommon', 1, 1, true),
+    ('Forest Bow', 'epic', 3, 25, true),
+    ('Mind Arrow', 'mythic', 2, 15, true),
+    ('Sword', 'rare', 1, 10, true);
